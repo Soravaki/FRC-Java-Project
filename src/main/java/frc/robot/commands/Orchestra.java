c5f3732b1f7a6414aabf9cd76ef3ef9fc9655696
git-Soravaki <justinpham838@gmail.com>
2022-04-13 14:00:47 -0500

    Updated Orchestra
    I dont know what I'm doing

--- src/main/java/frc/robot/commands/Orchestra.java
+++ src/main/java/frc/robot/commands/Orchestra.java
@@ -1,23 +1,32 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-/*
+
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
-import 
+public class Orchestra extends CommandBase {
+  /** Creates a new Orchestra. */
+  public Orchestra() {
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
 
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
 
-import frc.robot.subsystems.DriveTrain;
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
 
-/** Add your docs here. */
-/*
-public class Orchestra {
-    // orchestra object that holds all the instruments
-    Orchestra _Orchestra;
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
 
-    // Talon FX to play music through
-    Talon[] _fxes = { leftFrontTalon, rightFrontTalon };
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
-*/
