bb0e7806d79d778373a843daef70e7b1b93f2f15
git-Soravaki <justinpham838@gmail.com>
2022-04-14 14:13:42 -0500

    Orchestra Sadness
    We don't have Falcon 500s on the Robot :seal_cry:

--- src/main/java/frc/robot/commands/DriveOrchestra.java
+++ src/main/java/frc/robot/commands/DriveOrchestra.java
@@ -1,19 +1,20 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-/*
+
+// No Falcon 500's rest in peace
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import com.ctre.phoenix.music.Orchestra;
 
-public class Orchestra extends CommandBase {
+public class DriveOrchestra extends CommandBase {
 
-  Orchestra _orchestra;
+  DriveOrchestra _orchestra;
 
-  /** Creates a new Orchestra. */ /*
-  public Orchestra() {
+  /** Creates a new Orchestra. */ 
+  public DriveOrchestra() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
@@ -37,4 +38,3 @@ public class Orchestra extends CommandBase {
     return true;
   }
 }
-*/
